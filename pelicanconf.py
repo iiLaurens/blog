acf2cb0c19dfb78245f925ab352cea083bc68b9a
Laurens <git@laurens.xyz>
2016-05-01 10:33:55 +0200

    My initial blog

---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import os
+
+AUTHOR = u'Laurens'
+SITENAME = u'Laurens\' geeky adventures'
+#SITEURL = 'http://localhost:8000'
+RELATIVE_URLS = True
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_DATE_FORMAT = '%d-%m-%Y'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+FEED_ALL_RSS = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+STATIC_PATHS = ['images', 'files']
+
+# Blogroll
+LINKS = None
+
+HIDE_SIDEBAR = True
+
+BANNER = False
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = False
+
+DEFAULT_PAGINATION = 5
+
+TAG_CLOUD_MAX_ITEMS = 10
+
+DISPLAY_TAGS_ON_SIDEBAR = False
+
+THEME = "themes/Bootstrap3"
+
+PYGMENTS_STYLE = "zenburn"
+
+BOOTSTRAP_THEME = 'yeti'
+
+PLUGIN_PATHS = ['plugins']
+
+DIRECT_TEMPLATES = ('index', 'archives', 'search', 'tags')
+
+
+PLUGINS = ['tipue_search']
+
+DISQUS_SITENAME = 'iilaurens'
+#ADDTHIS_PROFILE = 'ra-520d4af6518bf3c7'
+
+
+ARTICLE_URL = 'blog/{slug}.html'
+ARTICLE_SAVE_AS = 'blog/{slug}.html'
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+TAG_URL = 'tags/{slug}.html'
+TAG_SAVE_AS = 'tags/{slug}.html'
+TAGS_URL = 'tags.html'
